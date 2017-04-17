0212a9e8e5d3abae441ff63d44dee8a2b1c6275c
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-04-17 15:51:14 -0600

    saving react file for later use

--- octo-system/src/page.js
+++ octo-system/src/page.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.svg';
 import './app.scss';
 
 class Page extends Component {
@@ -11,7 +11,7 @@ class Page extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          It's time to start programming!
         </p>
       </div>
     );
