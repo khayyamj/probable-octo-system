1d406cebeaf7958ab236e6214696d5c52c29bf6a
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-04-27 18:42:26 -0600

    stopping point

--- octo-system/src/ekkomedia/FullService.js
+++ octo-system/src/ekkomedia/FullService.js
@@ -4,10 +4,16 @@ export default class FullService extends Component {
   render () {
     return (
       <div className="FullService">
-        <h1>
+        <h2 className="full-service-words">
           Full Service <br />
           Digital Agency
+        </h2>
+        <hr className="hrBlueLine" />
+        <h1 className="full-service-number">
+          17
         </h1>
+        <p className="full-service-years"> Years </p>
+        <p className="full-service-game"> Playing The Digital Game </p>
       </div>
     )
   }
