99162776a4fd61a12dbab83ba0324efdf2a85669
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-05-01 19:17:11 -0600

    started excellence section

--- octo-system/src/ekkomedia/Excellence.js
+++ octo-system/src/ekkomedia/Excellence.js
@@ -4,9 +4,12 @@ export default class Excellence extends Component {
   render () {
     return (
       <div className="Excellence">
-        <p>
-          Excellence
-        </p>
+        <h3>
+          Industry Recognized Excellence
+        </h3>
+        <p>While we're proud of our work, we're more proud of the awards and industry recognition we've received over the years. </p>
+        <h1>13 </h1>
+        <h2>Awards </h2>
       </div>
     )
   }
